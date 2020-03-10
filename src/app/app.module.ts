99e794c82a23f7d447bd6068dd72f1b4d8c5d93f
hendrix <azzouz.m@outlook.net>
2020-03-10 11:30:04 +0100

    Todo List application développer via Ionic Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyADI1rEIt6NE9yJIv5UpAlRHtW2mphcQp8',
+  authDomain: 'todolist-9b4a6.firebaseapp.com',
+  databaseURL: 'https://todolist-9b4a6.firebaseio.com',
+  projectId: 'todolist-9b4a6',
+  storageBucket: 'todolist-9b4a6.appspot.com',
+  messagingSenderId: '856882024763',
+  appId: '1:856882024763:web:3ee44dbe5bbe407818a71e',
+  measurementId: 'G-D4VSTVJYMY'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(firebaseConfig),
+  AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +36,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
